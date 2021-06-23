02581e564fda575494e3eabf07234733f26cf236
jacknorrie <jacknorrie6785@gmail.com>
2021-06-23 01:03:02 +0100

    Now outputs tfrecord files.

--- 2_feature_extractor.py
+++ 2_feature_extractor.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
 import librosa
 
 def get_log_mel_spectrograms(waveforms, sample_rate=44100):
@@ -24,11 +24,70 @@ def get_log_mel_spectrograms(waveforms, sample_rate=44100):
     log_mel_spectrograms = np.stack(log_mel_spectrograms_list)
     return log_mel_spectrograms
 
-def feature_extractor(fpath, sample_rate=44100):
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))): # if value ist tensor
+        value = value.numpy() # get value of tensor
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-    waverforms = np.load(fpath)
+def _float_feature(value):
+    """Returns a floast_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def serialize_array(array):
+    array = tf.io.serialize_tensor(array)
+    return array
+
+def parse_single_image(image, label):
+    # define the dictionary -- the structure -- of our single example
+    data = {
+        'height': _int64_feature(image.shape[0]),
+        'width': _int64_feature(image.shape[1]),
+        'depth': _int64_feature(image.shape[2]),
+        'raw_image': _bytes_feature(serialize_array(image)),
+        'label': _int64_feature(label)
+    }
+    # create an Example, wrapping the single features
+    out = tf.train.Example(features=tf.train.Features(feature=data))
+
+    return out
+
+def write_images_to_tfr_short(images, labels, filename):
+    filename= filename+".tfrecords"
+    writer = tf.io.TFRecordWriter(filename) #create a writer that'll store our data to disk
+    count = 0
+
+    for index in range(len(images)):
+
+        #get the data we want to write
+        current_image = images[index]
+        current_label = labels[index]
+
+        out = parse_single_image(image=current_image, label=current_label)
+        writer.write(out.SerializeToString())
+        count += 1
+
+    writer.close()
+    print(f"Wrote {count} elements to TFRecord")
+    return count
+
+def fold_feature_extractor(fold, fpath, sample_rate=44100):
+    waverforms = np.load(fpath + f'/X_{fold}.npy')
+    labels = np.load(fpath + f'/y_{fold}.npy')
     log_mel_spectrograms = get_log_mel_spectrograms(waverforms,
                                                     sample_rate=sample_rate)
-    return log_mel_spectrograms
+    return log_mel_spectrograms[..., np.newaxis],\
+           np.squeeze(labels.astype(np.int64))
 
-S_dB = feature_extractor('Data/esc50_tabulated/X_1.npy')
+if __name__ == '__main__':
+    for fold in range(1, 5+1):
+        log_mel_spectrograms, labels =\
+            fold_feature_extractor(fold, 'Data/esc50_tabulated')
+        write_images_to_tfr_short(log_mel_spectrograms,
+                                  labels,
+                                  f'Data/esc50_tfrecords/fold_{fold}')
+
