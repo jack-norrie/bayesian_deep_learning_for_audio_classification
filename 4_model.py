12100d6590637c542bb310e23037bc8961937c45
jacknorrie <jacknorrie6785@gmail.com>
2021-09-01 22:38:58 +0100

    Different GPU.

--- 4_model.py
+++ 4_model.py
@@ -690,5 +690,5 @@ def train_wind_mel_cnn_insp():
 if __name__ == '__main__':
     # Set GPU to use:
     import os
-    os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     train_wind_mel_cnn_insp()
