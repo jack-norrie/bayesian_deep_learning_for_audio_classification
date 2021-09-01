e44d07153d0eb41912ce0b7e9ad06b8d41b5adad
jacknorrie <jacknorrie6785@gmail.com>
2021-09-01 14:49:47 +0100

    Increased shuffle buffer.

--- 4_model.py
+++ 4_model.py
@@ -99,7 +99,7 @@ def get_dataset(filenames, reader=lambda example:\
         read_waveform_tfrecord(example), batch_size = 16, classes=50):
     dataset = load_dataset(filenames, reader=reader)
     dataset = dataset.map(lambda x, y: (x, tf.one_hot(y, classes))) #OHE
-    dataset = dataset.shuffle(2048)
+    dataset = dataset.shuffle(16384)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     dataset = dataset.batch(batch_size)
     return dataset
