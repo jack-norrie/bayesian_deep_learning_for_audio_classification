98b60a329657ad52c8dc49b9f240f22597cd37c2
jacknorrie <jacknorrie6785@gmail.com>
2021-09-01 22:48:07 +0100

    Fixed loop bug.

--- 3_data_augmentation.py
+++ 3_data_augmentation.py
@@ -326,11 +326,12 @@ if __name__ == '__main__':
     import os
     os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     # Augment pure waveforms
-    data_augmentor(fpath_in=f'Data/esc50_wav_tfr/raw/fold_{i}.tfrecords',
-                   fpath_out=f'Data/esc50_wav_tfr/aug/fold_{i}.tfrecords',
-                   augmentor=sgn,
-                   augment_factor=10,
-                   output_shape=[1, 220500, 1])
+    for i in range(1, 6):
+        data_augmentor(fpath_in=f'Data/esc50_wav_tfr/raw/fold_{i}.tfrecords',
+                       fpath_out=f'Data/esc50_wav_tfr/aug/fold_{i}.tfrecords',
+                       augmentor=sgn,
+                       augment_factor=10,
+                       output_shape=[1, 220500, 1])
     windowed_mel_delta_extractor('Data/esc50_wav_tfr/aug/',
                                  'Data/esc50_mel_wind_tfr/aug/')
     """
