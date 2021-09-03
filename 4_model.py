9f2f2f15ebc983f23668be251a6102d282e971b9
jacknorrie <jacknorrie6785@gmail.com>
2021-09-03 11:54:10 +0100

    Changed to RMSprop optimizer.

--- 4_model.py
+++ 4_model.py
@@ -623,7 +623,7 @@ def train_wind_mel_cnn():
 
 def gen_wind_mel_cnn_insp(input_shape=(128, 128, 2), num_classes=50,
                      loss='categorical_crossentropy',
-                     optimizer=Adam(),
+                     optimizer=RMSprop(),
                      metrics=['accuracy'],
                      reg = 2.5e-2,
                      dor=0.5):
