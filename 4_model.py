79cc2e12491d058cf0fadf690f44fbf507c4834a
jacknorrie <jacknorrie6785@gmail.com>
2021-09-14 01:53:22 +0100

    Test.

--- 4_model.py
+++ 4_model.py
@@ -799,9 +799,9 @@ def train_wind_mel(batch_size, model_generator, epochs, fpath_id):
         # Generate model
         train_size = 0
         for batch in data_train:
-            print(batch)
-            return "lol"
+            train_size += batch.shape[0]
         print(train_size)
+        return "lol"
 
         model = model_generator(batch_size=len(list(data_train)))
 
